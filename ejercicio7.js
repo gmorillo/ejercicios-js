b9064ab1f3560855bec873728cd95c1af6df591e
Guillermo Morillo <morillo_guillermo@hotmail.com>
2024-06-04 14:42:25 +0200

    ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,9 @@
+function sum(numberOne , numberTwo) {
+  if(numberOne > numberTwo){
+    console.log(`el número más alto es ${numberOne}`);
+  }else{
+    console.log(`el número más alto es ${numberTwo}`);
+  }
+}
+
+sum (2, 4);
