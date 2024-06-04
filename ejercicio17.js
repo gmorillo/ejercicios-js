4719505c445579c816f44a03b0a63ed297e33807
Guillermo Morillo <morillo_guillermo@hotmail.com>
2024-06-04 18:24:59 +0200

    ejercicio 17

---
+++ ejercicio17.js
@@ -0,0 +1,19 @@
+/*
+
+Ejercicio 17
+Probando For...in: Usa un for...in 
+para imprimir por consola los datos del alienígena.. 
+Puedes usar este objeto:
+
+*/
+
+const alien = {
+  name: 'Wormuck',
+  race: 'Cucusumusu',
+  planet: 'Eden',
+  weight: '259kg'
+}
+
+for (const data in alien) {
+  console.log(`${data}: ${alien[data]}`);
+}
