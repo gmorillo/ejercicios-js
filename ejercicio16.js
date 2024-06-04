24e420e84c26fb9578c1ed6f10fc28d6dbd5e956
Guillermo Morillo <morillo_guillermo@hotmail.com>
2024-06-04 18:17:56 +0200

    ejercicio 16

---
+++ ejercicio16.js
@@ -0,0 +1,13 @@
+/*
+
+Ejercicio 16
+Probando For...of: Usa un bucle forof para recorrer todos los destinos del array. 
+Imprime en un console.log sus valores.
+
+*/
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+for (const destiny of placesToTravel) {
+  console.log(destiny);
+}
