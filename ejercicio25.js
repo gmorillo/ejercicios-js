a43ef09cd0e48640fdd637d6f160fbad0ff55b6f
Guillermo Morillo <morillo_guillermo@hotmail.com>
2024-06-05 11:39:04 +0200

    ejercicio 25

---
+++ ejercicio25.js
@@ -0,0 +1,20 @@
+/*
+  Ejercicio 24
+  Usa un bucle para sumar el total de las ventas (sellCount) de todos los productos.
+*/
+
+const products = [
+  { name: "Funko Dr. Strange", sellCount: 10 },
+  { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+  { name: "Sable laser FX", sellCount: 23 },
+  { name: "Varita de Voldemort", sellCount: 6 },
+];
+
+let totalSells = 0;
+for (let i = 0; i < products.length; i++) {
+  const element = products[i];
+  totalSells += element.sellCount;
+  average = totalSells / products.length;
+}
+
+console.log(`El promedio de ventas es de ${average}`);
