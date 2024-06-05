b286c3401b46819a7d725fd10580fcf2c152f078
Guillermo Morillo <morillo_guillermo@hotmail.com>
2024-06-05 11:35:43 +0200

    ejercicio 24

---
+++ ejercicio24.js
@@ -0,0 +1,19 @@
+/*
+  Ejercicio 24
+  Usa un bucle para sumar el total de las ventas (sellCount) de todos los productos.
+*/
+
+const products = [
+  { name: "Funko Dr. Strange", sellCount: 10 },
+  { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+  { name: "Sable laser FX", sellCount: 23 },
+  { name: "Varita de Voldemort", sellCount: 6 },
+];
+
+let totalSells = 0;
+
+for(const product of products){
+  totalSells += product.sellCount;
+}
+
+console.log(totalSells);
